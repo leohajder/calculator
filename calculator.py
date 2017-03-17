51938dab8dd016629aad155cceec35dc3a2eb90d
Leo Hajder <leohajder@gmail.com>
2017-03-17 10:25:24 +0100

    Update calculator.py
    Fix messagebox error, use app=Tk() convention

--- calculator.py
+++ calculator.py
@@ -1,25 +1,21 @@
-'''
-A simple calculator application made for fun and practice
-with Python 3.5.2 using the Tkinter module for GUI programming.
-Author: Leo Hajder (github.com/lhajder)
-'''
-
-#GUI module
 from tkinter import *
+from tkinter import messagebox
+
+about = "A simple calculator in Python 3.5.2 using the Tkinter module for GUI programming. \nAuthor: Leo Hajder (leohajder.github.io)"
 
 #initialization
-calculator = Tk()
-calculator.title("Calculator")
-calculator.resizable(width=False, height=False) #fixed 1st bug
+app = Tk()
+app.title("Calculator")
+app.resizable(width=False, height=False) #fixed 1st bug
 
 #menu
-menubar = Menu(calculator)
+menubar = Menu(app)
 filemenu = Menu(menubar, tearoff = 0)
-filemenu.add_command(label = "About", command = lambda: messagebox.showinfo("About", "A simple calculator application made for fun and practice with Python 3.5.2 using the Tkinter module for GUI programming. \nAuthor: Leo Hajder (leohajder.github.io)"))
+filemenu.add_command(label = "About", command = lambda: messagebox.showinfo("About", about))
 filemenu.add_separator()
-filemenu.add_command(label = "Exit", command = calculator.destroy)
+filemenu.add_command(label = "Exit", command = app.destroy)
 menubar.add_cascade(label = "File", menu = filemenu)
-calculator.config(menu=menubar)
+app.config(menu=menubar)
 
 #variables and functions
 storeBuffer = ""
@@ -81,39 +77,30 @@ def key(event):
     elif(event.keysym in ["c", "Escape"]):
         clear()
 
-calculator.bind("<Key>", key)
-
-#menu
-menubar = Menu(calculator)
-filemenu = Menu(menubar, tearoff = 0)
-filemenu.add_command(label = "About", command = lambda: messagebox.showwarning("About", "A simple calculator application made for fun and practice with Python 3.5.2 using the Tkinter module for GUI programming. \nAuthor: Leo Hajder (github.com/lhajder)"))
-filemenu.add_separator()
-filemenu.add_command(label = "Exit", command = calculator.destroy)
-menubar.add_cascade(label = "File", menu = filemenu)
-calculator.config(menu=menubar)
+app.bind("<Key>", key)
 
 #gui elements
-display = Entry(calculator, state = DISABLED, justify = RIGHT, bd = 2, disabledbackground = "pale green", textvariable = displayText).grid(row = 0, column = 0, columnspan = 4, ipadx = 5, ipady = 5, padx =5, pady = 5)
-btnClear = Button(calculator, text = "C", command = lambda: clear(), bg = "indian red", fg = "white").grid(row = 1, column = 0, ipadx = 5)
-btnDivide = Button(calculator, text = "/", command = lambda: click("/"), bg = "light blue", fg = "white").grid(row = 1, column = 1, ipadx = 5)
-btnMultiply = Button(calculator, text = "*", command = lambda: click("*"), bg = "light blue", fg = "white").grid(row = 1, column = 2, ipadx = 5)
-btnSubtract = Button(calculator, text = "-", command = lambda: click("-"), bg = "light blue", fg = "white").grid(row = 1, column = 3, ipadx = 5)
-btn7 = Button(calculator, text = "7", command = lambda: click("7")).grid(row = 2, column = 0, ipadx = 5, pady = 2)
-btn8 = Button(calculator, text = "8", command = lambda: click("8")).grid(row = 2, column = 1, ipadx = 5, pady = 2)
-btn9 = Button(calculator, text = "9", command = lambda: click("9")).grid(row = 2, column = 2, ipadx = 5, pady = 2)
-btnAdd = Button(calculator, text = "+", command = lambda: click("+"), bg = "light blue", fg = "white", height = 1, width = 1).grid(row = 2, column = 3, rowspan = 2, ipadx = 5, ipady = 14)
-btn4 = Button(calculator, text = "4", command = lambda: click("4")).grid(row = 3, column = 0, ipadx = 5, pady = 2)
-btn5 = Button(calculator, text = "5", command = lambda: click("5")).grid(row = 3, column = 1, ipadx = 5, pady = 2)
-btn6 = Button(calculator, text = "6", command = lambda: click("6")).grid(row = 3, column = 2, ipadx = 5, pady = 2)
-btn1 = Button(calculator, text = "1", command = lambda: click("1")).grid(row = 4, column = 0, ipadx = 5, pady = 2)
-btn2 = Button(calculator, text = "2", command = lambda: click("2")).grid(row = 4, column = 1, ipadx = 5, pady = 2)
-btn3 = Button(calculator, text = "3", command = lambda: click("3")).grid(row = 4, column = 2, ipadx = 5, pady = 2)
-btnEquals = Button(calculator, text = "=", command = lambda: click("="), bg = "light blue", fg = "white", height = 1, width = 1).grid(row = 4, column = 3, rowspan = 2, ipadx = 5, ipady = 14)
-btn0 = Button(calculator, text = "0", command = lambda: click("0")).grid(row = 5, column = 0, columnspan = 2, ipadx = 20, pady = 2)
-btnDecimal = Button(calculator, text = ",", command = lambda: click(".")).grid(row = 5, column = 2, ipadx = 7, pady = 2)
+display = Entry(app, state = DISABLED, justify = RIGHT, bd = 2, disabledbackground = "pale green", textvariable = displayText).grid(row = 0, column = 0, columnspan = 4, ipadx = 5, ipady = 5, padx =5, pady = 5)
+btnClear = Button(app, text = "C", command = lambda: clear(), bg = "indian red", fg = "white").grid(row = 1, column = 0, ipadx = 5)
+btnDivide = Button(app, text = "/", command = lambda: click("/"), bg = "light blue", fg = "white").grid(row = 1, column = 1, ipadx = 5)
+btnMultiply = Button(app, text = "*", command = lambda: click("*"), bg = "light blue", fg = "white").grid(row = 1, column = 2, ipadx = 5)
+btnSubtract = Button(app, text = "-", command = lambda: click("-"), bg = "light blue", fg = "white").grid(row = 1, column = 3, ipadx = 5)
+btn7 = Button(app, text = "7", command = lambda: click("7")).grid(row = 2, column = 0, ipadx = 5, pady = 2)
+btn8 = Button(app, text = "8", command = lambda: click("8")).grid(row = 2, column = 1, ipadx = 5, pady = 2)
+btn9 = Button(app, text = "9", command = lambda: click("9")).grid(row = 2, column = 2, ipadx = 5, pady = 2)
+btnAdd = Button(app, text = "+", command = lambda: click("+"), bg = "light blue", fg = "white", height = 1, width = 1).grid(row = 2, column = 3, rowspan = 2, ipadx = 5, ipady = 14)
+btn4 = Button(app, text = "4", command = lambda: click("4")).grid(row = 3, column = 0, ipadx = 5, pady = 2)
+btn5 = Button(app, text = "5", command = lambda: click("5")).grid(row = 3, column = 1, ipadx = 5, pady = 2)
+btn6 = Button(app, text = "6", command = lambda: click("6")).grid(row = 3, column = 2, ipadx = 5, pady = 2)
+btn1 = Button(app, text = "1", command = lambda: click("1")).grid(row = 4, column = 0, ipadx = 5, pady = 2)
+btn2 = Button(app, text = "2", command = lambda: click("2")).grid(row = 4, column = 1, ipadx = 5, pady = 2)
+btn3 = Button(app, text = "3", command = lambda: click("3")).grid(row = 4, column = 2, ipadx = 5, pady = 2)
+btnEquals = Button(app, text = "=", command = lambda: click("="), bg = "light blue", fg = "white", height = 1, width = 1).grid(row = 4, column = 3, rowspan = 2, ipadx = 5, ipady = 14)
+btn0 = Button(app, text = "0", command = lambda: click("0")).grid(row = 5, column = 0, columnspan = 2, ipadx = 20, pady = 2)
+btnDecimal = Button(app, text = ",", command = lambda: click(".")).grid(row = 5, column = 2, ipadx = 7, pady = 2)
 
 clear() #setup on load
 
-calculator.focus_force()
+app.focus_force()
 
-calculator.mainloop()
+app.mainloop()
